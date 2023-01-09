8d31520e00ed42b2f0c720e0484ff406c2c7d0d6
marcospaulo <marcospaulomacedo@gmail.com>
2023-01-09 01:05:15 -0300

    organizando arquivos

--- todos-cnais/js/scritp.js
+++ todos-cnais/js/scritp.js
@@ -1,8 +1,6 @@
 import {Pesquisar} from './pesquisar.js'
-import {Pilha} from './pilha.js'
 
 const pesquisar = new Pesquisar()
-const pilha = new Pilha()
 
 //let linkApi = 'https://servicodados.ibge.gov.br/api/v2/cnae/classes/'
 let linkApi = 'https://servicodados.ibge.gov.br/api/v2/cnae/subclasses/'
@@ -15,11 +13,9 @@ let options = {
 fetch(linkApi,options)
     .then(response => {response.json()
        .then(data => {(data)
-        //console.log(data)
+        console.log(data)
         pesquisar.buscarId(data)
-        pilha.push(data)
        })
     })
 .catch(e =>{console.log(`deu erro ${e}`)})
-
 
